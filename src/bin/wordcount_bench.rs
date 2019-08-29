6deee23478eb1e9616ef023da75e14dbf9c3260c
Lorenzo Selvatici <selvatici.lorenzo@gmail.com>
2019-08-29 12:53:20 +0200

    attempt with timers; need to use streaming harness

--- src/bin/wordcount_bench.rs
+++ src/bin/wordcount_bench.rs
@@ -1,18 +1,21 @@
 use timely::dataflow::{InputHandle, ProbeHandle};
 use timely::dataflow::operators::{Map, Inspect, Probe};
-use timely::dataflow::operators::aggregation::StateMachine;
 use dynamic_scaling_mechanism::state_machine::BinnedStateMachine;
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 use timely::dataflow::operators::broadcast::Broadcast;
 use timely::dataflow::operators::exchange::Exchange;
 use colored::Colorize;
-use rescaling_examples::{verify, LinesGenerator, LoadBalancer};
+use rescaling_examples::{LinesGenerator, LoadBalancer};
 use std::cell::RefCell;
 use dynamic_scaling_mechanism::{ControlInst, BinId, Control, BIN_SHIFT};
 use std::process::Command;
 use std::collections::VecDeque;
 use std::time::Instant;
+use timely::dataflow::operators::generic::operator::Operator;
+use timely::dataflow::channels::pact::Pipeline;
+use std::rc::Rc;
+use std::fs::File;
 
 const WORKER_BOOTSTRAP_MARGIN: u128 = 500 * 1_000_000; // 500 millis (as nanoseconds)
 
@@ -24,8 +27,6 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 
 fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
-        let should_verify = false;
-
         let mut lines_in = InputHandle::new();
         let mut control_in = InputHandle::new();
 
@@ -33,49 +34,58 @@ fn main() {
 
         let widx = worker.index();
 
-        worker.dataflow(|scope| {
+        let latencies1 = Rc::new(RefCell::new(Vec::<u128>::new()));
+        let latencies2 = Rc::clone(&latencies1);
 
+        let epoch_timer1 = Rc::new(Instant::now());
+        let epoch_timer2 = Rc::clone(&epoch_timer1);
+        let epoch_timer3 = Rc::clone(&epoch_timer1);
+
+        worker.dataflow::<u128,_,_>(|scope| {
             let rr = RefCell::new(0_u64);
 
             let words_in =
                 lines_in
                     .to_stream(scope)
                     .exchange(move |_| { let mut rr = rr.borrow_mut(); *rr+=1; *rr }) // round-robin
-                    .flat_map(|text: String|
+                    .flat_map(move |text: String| {
+                        let enter_time = epoch_timer1.elapsed().as_nanos();
                         text.split_whitespace()
-                            .map(move |word| (word.to_owned(), 1))
+                            .map(move |word| (word.to_owned(), (1, enter_time)))
                             .collect::<Vec<_>>()
-                    );
+                    });
 
             let control = control_in.to_stream(scope).broadcast();
 
             control.inspect(move |c| println!("[W{}] {}", widx, format!("control message is {:?}", c).bold().yellow()));
 
-            let stateful_out =
-                words_in
-                    .stateful_state_machine(|key: &String, val, agg: &mut u64| {
-                        *agg += val;
-                        (false, Some((key.clone(), *agg)))
-                    }, |key| calculate_hash(key), &control)
-                    .probe_with(&mut stateful_probe);
-
-            if should_verify {
-                let correct =
-                    words_in
-                        .state_machine(|key: &String, val, agg: &mut u64| {
-                            *agg += val;
-                            (false, Some((key.clone(), *agg)))
-                        }, |_key| 0); // need to send everything to worker 0 as number of peers changes and it would compute the wrong answer (no routing table without megaphone)
-
-                verify(&correct, &stateful_out);
-            }
+            words_in
+                .stateful_state_machine(|key: &String, val, agg: &mut (u64, u128)| {
+                    let (count, timestamp) = val;
+                    agg.0 += count;
+                    agg.1 = timestamp;
+                    (false, Some((key.clone(), *agg)))
+                }, |key| calculate_hash(key), &control)
+                .probe_with(&mut stateful_probe)
+                .sink(Pipeline, "delta_timestamps", move |input| {
+                    let mut _latencies_stash = latencies1.borrow_mut();
+                    let end_time = epoch_timer2.elapsed().as_nanos();
+                    while let Some((time, data)) = input.next() {
+                        for (_word, (_count, start_time)) in data.iter() {
+                            println!("start={:?} end={:?}", start_time, end_time);
+                            let latency = end_time - start_time;
+                            println!("{:?}:\t{:?}", *time.time(), latency);
+                        }
+                    }
+                });
         });
 
-        if worker.bootstrap() { return; }
+        if worker.bootstrap() {
+            while worker.step_or_park(None) {}
+            // TODO save_latencies()
+        }
 
         let mut lines_gen = LinesGenerator::new(1000, 10);
-
-        let epoch_timer = Instant::now();
 
         // epochs ~= ns
         let mut spawn_at_epochs = VecDeque::new();
@@ -94,7 +104,7 @@ fn main() {
         let mut load_balancer = LoadBalancer::new((0..worker.peers()).collect::<Vec<usize>>(), 1<<BIN_SHIFT);
 
         loop {
-            let epoch = epoch_timer.elapsed().as_nanos();
+            let epoch = epoch_timer3.elapsed().as_nanos();
 
             if widx == 0 {
                 if let Some(spawn_at_epoch) = spawn_at_epochs.front() {
@@ -103,7 +113,10 @@ fn main() {
 
                         let old_peers = worker.peers();
 
+                        let file = File::create("foo.txt").unwrap();
+
                         Command::new("cargo")
+                            .stdout(file)
                             .arg("run")
                             .arg("--bin")
                             .arg("wordcount_bench")
@@ -162,6 +175,7 @@ fn main() {
                 if bin_moved { spawn_info = None; }
             }
 
+            // TODO(lorenzo) maybe try streaming harness lib
             if epoch < 20*1_000_000_000 { // 20 seconds
                 lines_in.send(lines_gen.next());
             } else {
@@ -179,5 +193,7 @@ fn main() {
         control_in.close();
 
         worker.step_while(|| !stateful_probe.done());
+
+        // TODO save_latencies()
     }).unwrap();
 }
